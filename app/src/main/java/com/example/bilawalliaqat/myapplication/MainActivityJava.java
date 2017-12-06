81b69ca83c88ccab6a55ec726e06d563e0c2bbea
Bilawal Liaqat <bilawal.liaqat@nxb.com.pk>
2017-12-06 20:12:08 +0500

    App Theme Designing

--- app/src/main/java/com/example/bilawalliaqat/myapplication/MainActivityJava.java
+++ app/src/main/java/com/example/bilawalliaqat/myapplication/MainActivityJava.java
@@ -5,8 +5,6 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 /**
  * Created by bilawalliaqat on 06/12/2017.
@@ -14,20 +12,13 @@ import android.widget.Toast;
 
 public class MainActivityJava extends AppCompatActivity {
 
-    Button loginButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loginButton = (Button) findViewById(R.id.login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), R.string.login_success_message, Toast.LENGTH_LONG).show();
-            }
-        });
+
     }
 
 }
